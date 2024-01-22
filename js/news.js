2d8ba559d3253c105c77d28e27da1c6b27f22516
thanhbui27 <bthanh2001@gmail.com>
2024-01-22 23:53:14 +0700

    add slider

---
+++ js/news.js
@@ -0,0 +1,21 @@
+var slides = document.querySelectorAll(".sliders");
+var index = 0;
+
+function changeSlide() {
+  if (index < 0) {
+    index = slides.length - 1;
+  }
+
+  if (index > slides.length - 1) {
+    index = 0;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[index].style.display = "block";
+  index++;
+  setTimeout(changeSlide, 5000);
+}
+
+changeSlide();
